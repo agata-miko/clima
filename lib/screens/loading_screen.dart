a48564937d2b55c7ce68b6d099da4be9c25ab06c
mikol <mikolajek.a@gmail.com>
2023-02-22 10:10:53 +0100

    module 146 - (async/await) - http.get applied in getData function

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -16,8 +19,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    double latitude = location.latitude;
+    double longitude = location.longitude;
+    getData(latitude, longitude);
+  }
+
+  void getData(double latitude, double longitude) async {
+    http.Response response = await http.get(
+      Uri.parse(
+        'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/$latitude%2C%20$longitude?unitGroup=metric&key=HJULVPDK7GB24E6MDR5TJ9Q9V&contentType=json',
+      ),
+    );
+    print(response.body);
   }
 
   @override
