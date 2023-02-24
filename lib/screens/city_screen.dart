35d1b3af8a72091a774416eac5c1de6c25a87b2d
mikol <mikolajek.a@gmail.com>
2023-02-24 12:43:23 +0100

    module 153 - TextField created for the city (city_screen)

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -33,7 +33,10 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: const EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(style: TextStyle(color:Colors.black),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {},
+                ),
               ),
               TextButton(
                 onPressed: () {},
